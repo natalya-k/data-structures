b7d83b659fadaffb10920ea2c370ec71d711782f
natalya-k <nat-kasitsky@yandex.ru>
2017-08-22 15:09:49 +0300

    Update ArrayQueue.cs

--- ArrayQueue.cs
+++ ArrayQueue.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -23,69 +23,65 @@ namespace DataStructures
 
         public void Enqueue(T data)
         {
-            if (!IsFull)
+            if (IsFull)
             {
-                items[last++] = data;                
-
-                if (last == items.Length)
-                {
-                    last = 0;
-                }
-
-                Count++;
+                throw new InvalidOperationException("Queue overflow.");
             }
-            else
+
+            items[last++] = data;
+
+            if (last == items.Length)
             {
-                throw new Exception("Queue overflow.");
+                last = 0;
             }
+
+            Count++;
         }
 
         public T Dequeue()
         {
-            if (!IsEmpty)
+            if (IsEmpty)
             {
-                T result = items[first];
-
-                items[first++] = default(T);
-
-                if (first == items.Length)
-                {
-                    first = 0;
-                }
-
-                Count--;
-
-                return result;
+                throw new InvalidOperationException("Queue underflow.");
             }
-            else
+            
+            T result = items[first];
+
+            items[first++] = default(T);
+
+            if (first == items.Length)
             {
-                throw new Exception("Queue underflow.");
+                first = 0;
             }
+
+            Count--;
+
+            return result;
         }
 
         public T Peek()
         {
-            if (!IsEmpty)
+            if (IsEmpty)
             {
-                return items[first];
+                throw new InvalidOperationException("The queue is empty.");
             }
-            else
-            {
-                throw new Exception("The queue is empty.");
-            }
+            
+            return items[first];            
         }
 
         public void Print()
         {
             foreach (T item in this)
             {
-                Console.WriteLine(item.ToString());
+                Console.Write("{0} ", item.ToString());
             }
+
+            Console.WriteLine();
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            for (int i = first; i < last; i++)
+            for (int i = first; i < Count; i++)
             {
                 yield return items[i];
             }
