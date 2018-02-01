e1b014105a8c723ac906600add1d7bafbc1f639e
natalya-k <nat-kasitsky@yandex.ru>
2018-02-01 11:19:43 +0300

    Update BinaryTree.cs

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace DataStructures
 {
-    class BinaryTree<T> : IEnumerable<T>
+    class BinaryTree<T> : IEnumerable<T> 
+        where T : IComparable
     {
         public bool IsEmpty { get { return (Root == null); } }
 
@@ -24,6 +25,21 @@ namespace DataStructures
             tree.Root.AddRight(3);
             tree.Root.Left.AddLeft(4);
             tree.Root.Left.AddRight(5);
+
+            return tree;
+        }
+
+        public static BinaryTree<int> SampleBSTree()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>(4);
+            tree.Root.AddLeft(2);
+            tree.Root.AddRight(5);
+            tree.Root.Left.AddLeft(1);
+            tree.Root.Left.AddRight(3);
+            tree.Root.Right.AddRight(6);
+            tree.Root.Right.Right.AddRight(7);
+            tree.Root.Right.Right.Right.AddRight(8);
+
             return tree;
         }
 
@@ -55,52 +71,63 @@ namespace DataStructures
             LevelOrderTraversal(Root);
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public int MaxWidth()
         {
-            //нерекурсивная реализация inorder traversal
-
             if (IsEmpty)
             {
-                yield return default(T);
-            }
+                return 0;
+            }                
 
-            Stack<Node> stack = new Stack<Node>();
+            int result = 0;
 
-            Node current = Root;
+            Queue<Node> queue = new Queue<Node>();
 
-            bool goLeftNext = true;
+            queue.Enqueue(Root);
 
-            stack.Push(current);
-
-            while (stack.Count > 0)
+            while (queue.Count > 0)
             {
-                if (goLeftNext)
+                int count = queue.Count;
+
+                if (count > result)
                 {
-                    while (current.Left != null)
+                    result = count;
+                }
+
+                while (count > 0)
+                {
+                    Node current = queue.Dequeue();
+
+                    if (current.Left != null)
                     {
-                        stack.Push(current);
-                        current = current.Left;
+                        queue.Enqueue(current.Left);
                     }
-                }
 
-                yield return current.Key;
+                    if (current.Right != null)
+                    {
+                        queue.Enqueue(current.Right);
+                    }
 
-                if (current.Right != null)
-                {
-                    current = current.Right;
-                    goLeftNext = true;
-                }
-                else
-                {
-                    current = stack.Pop();
-                    goLeftNext = false;
+                    count--;
                 }
             }
+
+            return result;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public int MaxHeight()
         {
-            return GetEnumerator();
+            return MaxHeight(Root);
+        }
+
+        public bool IsBalanced()
+        {
+            int height = 0;
+            return IsBalanced(Root, ref height);
+        }
+
+        public bool IsBST()
+        {
+            return IsBST(Root, null, null);
         }
 
         protected virtual void PrintNode(Node node)
@@ -177,6 +204,125 @@ namespace DataStructures
                     queue.Enqueue(current.Right);
                 }
             }
+        }
+
+        private int MaxHeight(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            int leftHeight = MaxHeight(node.Left);
+            int rightHeight = MaxHeight(node.Right);
+
+            int maxHeight = Math.Max(leftHeight, rightHeight);
+
+            return maxHeight + 1;
+        }
+
+        private bool IsBalanced(Node node, ref int height)
+        {
+            if (node == null)
+            {
+                height = 0;
+                return true;
+            }
+
+            int leftHeight = 0;
+            int rightHeight = 0;
+
+            bool leftBalanced = IsBalanced(node.Left, ref leftHeight);
+            bool rightBalanced = IsBalanced(node.Right, ref rightHeight);
+
+            height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+
+            if (Math.Abs(leftHeight - rightHeight) >= 2)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsBST(Node node, Node leftParent, Node rightParent)
+        {
+            if (node == null)
+            {
+                return true;
+            }
+            
+            if (leftParent != null)
+            {
+                int compared = node.Key.CompareTo(leftParent.Key);
+
+                if (compared < 0)
+                {
+                    return false;
+                }
+            }
+
+            if (rightParent != null)
+            {
+                int compared = node.Key.CompareTo(rightParent.Key);
+
+                if (compared > 0)
+                {
+                    return false;
+                }
+            }
+
+            return 
+                IsBST(node.Left, leftParent, node) && 
+                IsBST(node.Right, node, rightParent);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            //нерекурсивная реализация inorder traversal
+
+            if (IsEmpty)
+            {
+                yield return default(T);
+            }
+
+            Stack<Node> stack = new Stack<Node>();
+
+            Node current = Root;
+
+            bool goLeftNext = true;
+
+            stack.Push(current);
+
+            while (stack.Count > 0)
+            {
+                if (goLeftNext)
+                {
+                    while (current.Left != null)
+                    {
+                        stack.Push(current);
+                        current = current.Left;
+                    }
+                }
+
+                yield return current.Key;
+
+                if (current.Right != null)
+                {
+                    current = current.Right;
+                    goLeftNext = true;
+                }
+                else
+                {
+                    current = stack.Pop();
+                    goLeftNext = false;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 
         protected class Node
